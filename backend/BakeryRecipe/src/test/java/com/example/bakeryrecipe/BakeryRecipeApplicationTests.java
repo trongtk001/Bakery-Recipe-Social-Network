debce11639971e19a26c6c72d194defc13c74a67
Trong <huynhtranthetrong@example.com>
2022-09-20 00:16:35 +0700

    create project base

---
+++ backend/BakeryRecipe/src/test/java/com/example/bakeryrecipe/BakeryRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakeryrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
