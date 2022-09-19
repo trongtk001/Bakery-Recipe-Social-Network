793e2096bb9641a9ccbad912b911063f7db443a9
Trong <huynhtranthetrong@example.com>
2022-09-20 00:16:35 +0700

    create project base

---
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/BakeryRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakeryrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryRecipeApplication.class, args);
+    }
+
+}
