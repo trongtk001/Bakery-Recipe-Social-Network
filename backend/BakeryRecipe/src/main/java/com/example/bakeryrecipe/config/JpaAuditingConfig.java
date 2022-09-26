f06ce5723e113d2aa978f5e43cc49a389dc54fb0
viethungdev23 <viethungdev23@gmail.com>
2022-09-26 16:16:36 +0700

    create post fuction

--- backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/config/JpaAuditingConfig.java
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/config/JpaAuditingConfig.java
@@ -1,19 +1,36 @@
 package com.example.bakeryrecipe.config;
 
-import com.example.bakeryrecipe.auditor.AuditorAwareImpl;
+import com.example.bakeryrecipe.entity.Member;
+import com.example.bakeryrecipe.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfig {
 
 	@Bean
-    public AuditorAware<String> auditorProvider() {
+    public AuditorAware<Member> auditorProvider() {
         return new AuditorAwareImpl();
     }
 
 
+}
+
+class AuditorAwareImpl implements AuditorAware<Member> {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Override
+    public Optional<Member> getCurrentAuditor() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return memberRepository.findOneByUsername(username);
+    }
 }
