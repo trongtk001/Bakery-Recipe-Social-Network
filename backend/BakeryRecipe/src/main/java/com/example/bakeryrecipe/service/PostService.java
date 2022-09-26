603a840cd961abf66c008b95f0d2f924eae56205
viethungdev23 <viethungdev23@gmail.com>
2022-09-26 16:16:36 +0700

    create post fuction

--- backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/service/PostService.java
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.bakeryrecipe.service;
 
 import com.example.bakeryrecipe.dto.PostDTO;
+import com.example.bakeryrecipe.entity.Post;
 
 public interface PostService extends Service<PostDTO>{
 
