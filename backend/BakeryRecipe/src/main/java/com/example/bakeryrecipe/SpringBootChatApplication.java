c7cbf6bf222841f8efe36e7ef6b107d002bba987
Addison99 <sonluong995@gmail.com>
2022-09-29 10:26:09 +0700

    Message Chatbot

---
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/SpringBootChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakeryrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringBootChatApplication.class , args);
+    }
+}
