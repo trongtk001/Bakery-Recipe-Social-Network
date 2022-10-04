e5bac6ba7601ccfa04c131feabd8d7ffa32eedaa
Huynh Tran The Trong <huynhtranthetrong@gmail.com>
2022-10-04 23:32:46 +0700

    update search member and search post

---
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/api/output/ListMemberOutput.java
@@ -0,0 +1,55 @@
+package com.example.bakeryrecipe.api.output;
+
+import com.example.bakeryrecipe.dto.MemberDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMemberOutput {
+    private int page;
+    private int size;
+    private int totalPage;
+    private List<MemberDTO> list = new ArrayList<>();
+
+    public ListMemberOutput() {
+    }
+
+    public ListMemberOutput(int page, int size, int totalPage, List<MemberDTO> list) {
+        this.page = page;
+        this.size = size;
+        this.totalPage = totalPage;
+        this.list = list;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<MemberDTO> getList() {
+        return list;
+    }
+
+    public void setList(List<MemberDTO> list) {
+        this.list = list;
+    }
+}
---
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/api/output/ListPostOutput.java
@@ -0,0 +1,55 @@
+package com.example.bakeryrecipe.api.output;
+
+import com.example.bakeryrecipe.dto.PostDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPostOutput {
+    private int page;
+    private int size;
+    private int totalPage;
+    private List<PostDTO> list = new ArrayList<>();
+
+    public ListPostOutput() {
+    }
+
+    public ListPostOutput(int page, int size, int totalPage, List<PostDTO> list) {
+        this.page = page;
+        this.size = size;
+        this.totalPage = totalPage;
+        this.list = list;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<PostDTO> getList() {
+        return list;
+    }
+
+    public void setList(List<PostDTO> list) {
+        this.list = list;
+    }
+}
