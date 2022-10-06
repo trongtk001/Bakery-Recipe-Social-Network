1c921243475ddfa1fc9378ffbd56d910c804b958
Huynh Tran The Trong <huynhtranthetrong@gmail.com>
2022-10-06 14:40:08 +0700

    add ingredient service

--- backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/api/PostAPI.java
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/api/PostAPI.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/post")
 @PreAuthorize("permitAll()")
