514e8cc2a67663b747694e568ef9dceccbe5589f
Addison99 <sonluong995@gmail.com>
2022-10-15 18:51:30 +0700

    Chatbot

--- backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/config/WebSocketChatEventListener.java
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/config/WebSocketChatEventListener.java
@@ -1,5 +1,8 @@
 package com.example.bakeryrecipe.config;
 
+import com.example.bakeryrecipe.domain.WebSocketChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -7,14 +10,14 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
-import com.example.bakeryrecipe.domain.WebSocketChatMessage;
 /**
  *
  * @author sonluong
  */
 @Component
 public class WebSocketChatEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketChatEventListener.class);
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
 
@@ -29,6 +32,7 @@ public class WebSocketChatEventListener {
 
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if(username != null) {
+            logger.info("User Disconnected : " + username);
 
             WebSocketChatMessage chatMessage = new WebSocketChatMessage();
             chatMessage.setType("Leave");
