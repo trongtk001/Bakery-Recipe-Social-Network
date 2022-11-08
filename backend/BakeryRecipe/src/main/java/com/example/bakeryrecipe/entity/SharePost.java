e3587b9a0fbce7972d705fa58f3c8fa52f642e16
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2022-11-09 01:56:43 +0700

    update

---
+++ backend/BakeryRecipe/src/main/java/com/example/bakeryrecipe/entity/SharePost.java
@@ -0,0 +1,50 @@
+package com.example.bakeryrecipe.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class SharePost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "share_post_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
--- backend/BakeryRecipe/src/test/java/com/example/bakeryrecipe/BakeryRecipeApplicationTests.java
+++ backend/BakeryRecipe/src/test/java/com/example/bakeryrecipe/BakeryRecipeApplicationTests.java
@@ -1,16 +1,31 @@
 package com.example.bakeryrecipe;
 
+import com.example.bakeryrecipe.dto.PostDTO;
+import com.example.bakeryrecipe.entity.Post;
 import com.example.bakeryrecipe.repository.CommentRepository;
-import com.example.bakeryrecipe.repository.MemberRepository;
-import com.example.bakeryrecipe.service.MemberService;
+import com.example.bakeryrecipe.repository.PostRepository;
+import com.example.bakeryrecipe.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class BakeryRecipeApplicationTests {
 
     @Autowired
     CommentRepository commentRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    PostService postService;
+
+    @Test
+    @Transactional
+    void test() {
+        PostDTO postDTO = postService.search(41L);
+    }
 
 }
