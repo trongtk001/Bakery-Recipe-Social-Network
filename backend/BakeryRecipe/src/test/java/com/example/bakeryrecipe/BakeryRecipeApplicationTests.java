e8f45fe9b0e3e5a760e9bde2c96cc362c9837acd
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2022-11-09 01:08:44 +0700

    Update BakeryRecipeApplicationTests.java

--- backend/BakeryRecipe/src/test/java/com/example/bakeryrecipe/BakeryRecipeApplicationTests.java
+++ backend/BakeryRecipe/src/test/java/com/example/bakeryrecipe/BakeryRecipeApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.bakeryrecipe;
 
 import com.example.bakeryrecipe.repository.CommentRepository;
+import com.example.bakeryrecipe.repository.MemberRepository;
+import com.example.bakeryrecipe.service.MemberService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +12,5 @@ class BakeryRecipeApplicationTests {
 
     @Autowired
     CommentRepository commentRepository;
-
 
 }
